51482025c5a088cc95f82e1e215512698a20ab98
Ssantaj <samuelsanta2004@gmail.com>
2023-11-16 01:13:01 -0500

    script de datos de prueba

--- Datos/script.sql
+++ Datos/script.sql
@@ -130,3 +130,42 @@ begin
    	where p.id=p_id;
 end;
 $$;
+
+----------------------------------------------------------------------------------------
+
+-- Insertar datos en la tabla de sedes
+INSERT INTO sedes (nombre, direccion, nombre_admin, contacto_admin, telefono_admin, ciudad)
+VALUES
+  ('Sede Principal', 'Calle de los licores #123', 'Javier Gómez', 'javier@example.com', '1234567890', 'Bogotá'),
+  ('Sede Norte', 'Avenida del Ron #789', 'Ana Martínez', 'ana@example.com', '0987654321', 'Medellín'),
+  ('Sede Sur', 'Carrera del Tequila #456', 'Carlos González', 'carlos@example.com', '1122334455', 'Cali');
+
+-- Insertar datos en la tabla de tipos
+INSERT INTO tipos (descripcion)
+VALUES
+  ('Whisky'),
+  ('Vodka'),
+  ('Ron'),
+  ('Tequila'),
+  ('Ginebra');
+
+-- Insertar datos en la tabla de proveedores
+INSERT INTO proveedores (nombre_empresa, responsable, correo, numero_registro, numero_contacto, direccion_empresa, ciudad)
+VALUES
+  ('WhiskyWorld', 'Luis Hernández', 'luis@whiskyworld.com', 123456, '0987654321', 'Carrera 20 #45-67', 'Bogotá'),
+  ('RonExquisite', 'María Rodríguez', 'maria@ronexquisite.com', 789012, '1122334455', 'Calle 80 #12-34', 'Medellín'),
+  ('TequilaLandia', 'Pedro Gómez', 'pedro@tequilalandia.com', 345678, '5432167890', 'Avenida 40 #98-76', 'Cali');
+
+-- Insertar datos en la tabla de usuarios
+INSERT INTO usuarios (correo, contrasena, rol, sede_id)
+VALUES
+  ('admin@licores.com', 'contraseña123', 'Administrador', 1),
+  ('usuario1@licores.com', 'pass123', 'Usuario', 2),
+  ('usuario2@licores.com', 'password123', 'Usuario', 3);
+
+-- Insertar datos en la tabla de productos
+INSERT INTO productos (nombre, tipo_id, tamaño, imagen, precio_base, precio_venta, proveedor_id)
+VALUES
+  ('Johnnie Walker Black Label', 1, '750ml', 'https://static.wixstatic.com/media/477dc5_f3e8aa2c49de486f8b6b6a0e0c736842~mv2.png/v1/fill/w_560,h_560,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/477dc5_f3e8aa2c49de486f8b6b6a0e0c736842~mv2.png', 150000, 175000, 1),
+  ('Havana Club Añejo 7 Años', 3, '700ml', 'https://d2j6dbq0eux0bg.cloudfront.net/images/30491376/1617549085.jpg', 80000, 95000, 2),
+  ('Patrón Reposado', 4, '750ml', 'https://licoresmedellin.com/cdn/shop/products/tequila-patron-reposado-botella-700mltequila-patron-reposado-botella-700mlpatronlicores-medellin-658858.png?crop=center&height=600&v=1681662091&width=600', 120000, 140000, 3);
